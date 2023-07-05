8755d29c74f3b54daa16e9f08cce045c552d711e
AlipourIm <iman.alip2001@gmail.com>
2023-07-05 16:05:34 +0330

    implement proof of work

--- src/blockchain.py
+++ src/blockchain.py
@@ -7,16 +7,17 @@ from Crypto.Hash import keccak
 class Blockchain:
     """Defines a blockchain on an individual machine"""
     def __init__(self) -> None:
+        self.hardness = 4
         self.chain = []
         self.transactions = []
         self.new_block(proof=100, previous_hash=1)
 
-    def new_block(self, proof, previous_hash=None):
+    def new_block(self, proof_of_work, previous_hash=None):
         """ Create a new block """
         block = {"index": len(self.chain) + 1,
                  "time_stamp": time(),
                  "transactions": self.transactions,
-                 "proof": proof,
+                 "proof_of_work": proof_of_work,
                  "previous_hash": previous_hash or self.hash(self.chain[-1])
                 }
         self.transactions = []
@@ -27,16 +28,37 @@ class Blockchain:
         """Add a new tranaction to memopool"""
         self.transactions.append({"sender": sender, "receiver": receiver, "amount": amount})
         return self.last_block["index"] + 1
-
+        
     @staticmethod
-    def hash(block):
+    def get_hash(string: str):
+        """Returns the Keccak hash of a string"""
+        tmp = keccak.new(digest_bits=256)
+        tmp.update(string)
+        return tmp.hexdigest()
+    
+    @staticmethod
+    def get_block_hash(block):
         """Get hash of a block with Keccak hash function"""
         block_string = json.dumps(block, sort_keys=True).encode()
-        k = keccak.new(digest_bits=256)
-        k.update(block_string)
-        return k.hexdigest()
+        return Blockchain.get_hash(block_string)
 
     @property
     def last_block(self):
         """Get last block"""
         pass
+
+    @staticmethod
+    def proof_of_work_is_valid(self, previous_proof_of_work, proof_of_work):
+        """Checks if given proof of work is valid"""
+        to_be_checked = f"\"previous_proof_of_work\": {str(previous_proof_of_work)}, \"proof_of_work\": {str(proof_of_work)}"
+        hash_of_to_be_checked = Blockchain.get_hash(to_be_checked)
+        return hash_of_to_be_checked[:self.hardness] == "0" * self.hardness
+
+    def proof_of_work(self, previous_proof_of_work):
+        """Shows that work has been done by miner"""
+        proof_of_work = 0
+        while not Blockchain.proof_of_work_is_valid(previous_proof_of_work, proof_of_work):
+            proof_of_work += 1
+        
+        return proof_of_work
+
